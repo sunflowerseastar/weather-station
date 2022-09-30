4b7e506b08de9680348879c1b186a7dc14644df7
Grant Surlyn <grant@sunflowerseastar.com>
2022-09-29 23:12:45 -0400

    revise book's VEML6030 h/w settings to VCNL4040 h/w

--- sensor_hub_poncho/vcnl4040/lib/vcnl4040/config.ex
+++ sensor_hub_poncho/vcnl4040/lib/vcnl4040/config.ex
@@ -1,6 +1,5 @@
 defmodule VCNL4040.Config do
-  defstruct gain: :gain_1_4th,
-            int_time: :it_100_ms,
+  defstruct int_time: :it_80_ms,
             shutdown: false,
             interrupt: false
 
@@ -11,33 +10,22 @@ defmodule VCNL4040.Config do
     reserved = 0
     persistence_protect = 0
 
-    <<integer::16>> = <<
-      reserved::3,
-      gain(config.gain)::2,
-      reserved::1,
-      int_time(config.int_time)::4,
-      persistence_protect::2,
+    <<integer::8>> = <<
+      int_time(config.int_time)::2, # ALS_IT
       reserved::2,
-      interrupt(config.interrupt)::1,
-      shutdown(config.shutdown)::1
+      persistence_protect::2, # ALS_PERS
+      interrupt(config.interrupt)::1, # ALS_INT_EN
+      shutdown(config.shutdown)::1 # ALS_SD
     >>
 
     integer
   end
 
-  defp gain(:gain_1x), do: 0b0
-  defp gain(:gain_2x), do: 0b01
-  defp gain(:gain_1_8th), do: 0b10
-  defp gain(:gain_1_4th), do: 0b11
-  defp gain(:gain_default), do: 0b11
-
-  defp int_time(:it_25_ms), do: 0b1100
-  defp int_time(:it_50_ms), do: 0b1000
-  defp int_time(:it_100_ms), do: 0b0000
-  defp int_time(:it_200_ms), do: 0b0001
-  defp int_time(:it_400_ms), do: 0b0010
-  defp int_time(:it_800_ms), do: 0b0011
-  defp int_time(:it_default), do: 0b0000
+  defp int_time(:it_80_ms), do: 0b00
+  defp int_time(:it_160_ms), do: 0b01
+  defp int_time(:it_320_ms), do: 0b10
+  defp int_time(:it_640_ms), do: 0b11
+  defp int_time(:it_default), do: 0b00
 
   defp shutdown(true), do: 1
   defp shutdown(_), do: 0
@@ -45,36 +33,14 @@ defmodule VCNL4040.Config do
   defp interrupt(true), do: 1
   defp interrupt(_), do: 0
 
-  # https://github.com/akoutmos/nerves_weather_station
   # https://github.com/akoutmos/nerves_weather_station/blob/master/nerves_code/veml6030/lib/veml6030/config.ex
-  @to_lumens_factor %{
-    {:it_800_ms, :gain_2x} => 0.0036,
-    {:it_800_ms, :gain_1x} => 0.0072,
-    {:it_800_ms, :gain_1_4th} => 0.0288,
-    {:it_800_ms, :gain_1_8th} => 0.0576,
-    {:it_400_ms, :gain_2x} => 0.0072,
-    {:it_400_ms, :gain_1x} => 0.0144,
-    {:it_400_ms, :gain_1_4th} => 0.0576,
-    {:it_400_ms, :gain_1_8th} => 0.1152,
-    {:it_200_ms, :gain_2x} => 0.0144,
-    {:it_200_ms, :gain_1x} => 0.0288,
-    {:it_200_ms, :gain_1_4th} => 0.1152,
-    {:it_200_ms, :gain_1_8th} => 0.2304,
-    {:it_100_ms, :gain_2x} => 0.0288,
-    {:it_100_ms, :gain_1x} => 0.0576,
-    {:it_100_ms, :gain_1_4th} => 0.2304,
-    {:it_100_ms, :gain_1_8th} => 0.4608,
-    {:it_50_ms, :gain_2x} => 0.0576,
-    {:it_50_ms, :gain_1x} => 0.1152,
-    {:it_50_ms, :gain_1_4th} => 0.4608,
-    {:it_50_ms, :gain_1_8th} => 0.9216,
-    {:it_25_ms, :gain_2x} => 0.1152,
-    {:it_25_ms, :gain_1x} => 0.2304,
-    {:it_25_ms, :gain_1_4th} => 0.9216,
-    {:it_25_ms, :gain_1_8th} => 1.8432
-  }
+  # @to_lumens_factor %{
+  #   {:it_800_ms, :gain_2x} => 0.0036,
+  #   {:it_800_ms, :gain_1x} => 0.0072,
+  #   ...
+  # }
 
-  def to_lumens(config, measurement) do
-    @to_lumens_factor[{config.int_time, config.gain}] * measurement
-  end
+  # def to_lumens(config, measurement) do
+  #   @to_lumens_factor[{config.int_time, config.gain}] * measurement
+  # end
 end
