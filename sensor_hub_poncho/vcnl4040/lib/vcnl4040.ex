9095feb9d686164f1a91b657172d4ef5eda31e5b
Grant Surlyn <grant@sunflowerseastar.com>
2022-09-29 23:40:48 -0400

    wrap core and h/w in GenServer

--- sensor_hub_poncho/vcnl4040/lib/vcnl4040.ex
+++ sensor_hub_poncho/vcnl4040/lib/vcnl4040.ex
@@ -1,18 +1,66 @@
-defmodule Vcnl4040 do
-  @moduledoc """
-  Documentation for `Vcnl4040`.
-  """
+defmodule VCNL4040 do
+  use GenServer
+  require Logger
+  alias VCNL4040.{Comm, Config}
 
-  @doc """
-  Hello world.
+  @impl true
+  def init(%{address: address, i2c_bus_name: bus_name} = args) do
+    i2c = Comm.open(bus_name)
 
-  ## Examples
+    config =
+      args
+      |> Map.take([:int_time, :shutdown, :interrupt])
+      |> Config.new()
 
-      iex> Vcnl4040.hello()
-      :world
+    Comm.write_config(config, i2c, address)
+    :timer.send_interval(1_000, :measure)
 
-  """
-  def hello do
-    :world
+    state = %{
+      i2c: i2c,
+      address: address,
+      config: config,
+      last_reading: :no_reading
+    }
+
+    {:ok, state}
+  end
+
+  def init(args) do
+    {bus_name, address} = Comm.discover()
+    transport = "bus: #{bus_name}, address: #{address}"
+
+    Logger.info("Starting VCNL4040. Please specify an address and a bus.")
+    Logger.info("Starting on " <> transport)
+
+    defaults =
+      args
+      |> Map.put(:address, address)
+      |> Map.put(:i2c_bus_name, bus_name)
+
+    init(defaults)
+  end
+
+  @impl true
+  def handle_info(
+        :measure,
+        %{i2c: i2c, address: address, config: config} = state
+      ) do
+    last_reading = Comm.read(i2c, address, config)
+    updated_with_reading = %{state | last_reading: last_reading}
+
+    {:noreply, updated_with_reading}
+  end
+
+  @impl true
+  def handle_call(:get_measurement, _from, state) do
+    {:reply, state.last_reading, state}
+  end
+
+  def start_link(options \\ %{}) do
+    GenServer.start_link(__MODULE__, options, name: __MODULE__)
+  end
+
+  def get_measurement do
+    GenServer.call(__MODULE__, :get_measurement)
   end
 end
