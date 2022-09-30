b351f5ccf30f4174579cc6240300f211ebddc5bc
Grant Surlyn <grant@sunflowerseastar.com>
2022-09-29 22:33:23 -0400

    mix new vcnl4040

---
+++ sensor_hub_poncho/vcnl4040/lib/vcnl4040.ex
@@ -0,0 +1,18 @@
+defmodule Vcnl4040 do
+  @moduledoc """
+  Documentation for `Vcnl4040`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vcnl4040.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ sensor_hub_poncho/vcnl4040/test/vcnl4040_test.exs
@@ -0,0 +1,8 @@
+defmodule Vcnl4040Test do
+  use ExUnit.Case
+  doctest Vcnl4040
+
+  test "greets the world" do
+    assert Vcnl4040.hello() == :world
+  end
+end
