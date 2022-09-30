131d90eeac44d0e7f11175c9a14c7c100e286deb
Grant Surlyn <grant@sunflowerseastar.com>
2022-09-30 00:16:40 -0400

    add sensor_hub deps for ccs811 and bme280

--- sensor_hub_poncho/sensor_hub/mix.exs
+++ sensor_hub_poncho/sensor_hub/mix.exs
@@ -40,6 +40,8 @@ defmodule SensorHub.MixProject do
       {:nerves_runtime, "~> 0.13.0", targets: @all_targets},
       {:nerves_pack, "~> 0.7.0", targets: @all_targets},
       {:vcnl4040, path: "../vcnl4040", targets: @all_targets},
+      {:bmp280, "~> 0.2.11", targets: @all_targets},
+      {:ccs811, "~> 0.2.0", targets: @all_targets},
 
       # Dependencies for specific targets
       # NOTE: It's generally low risk and recommended to follow minor version
