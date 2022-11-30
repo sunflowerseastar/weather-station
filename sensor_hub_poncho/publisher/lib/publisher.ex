c9ccc5ae2271c62d541efd40a78b59d58651c372
Grant Surlyn <grant@sunflowerseastar.com>
2022-11-30 10:11:02 -0500

    create new publisher subproject: `mix new publisher`

---
+++ sensor_hub_poncho/publisher/lib/publisher.ex
@@ -0,0 +1,18 @@
+defmodule Publisher do
+  @moduledoc """
+  Documentation for `Publisher`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Publisher.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
