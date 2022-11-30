864542abdbd8a8dc89961eff38c5ac25b6ab1dd4
Grant Surlyn <grant@sunflowerseastar.com>
2022-11-30 10:25:38 -0500

    add publish handle_info, measure, publish

--- sensor_hub_poncho/publisher/lib/publisher.ex
+++ sensor_hub_poncho/publisher/lib/publisher.ex
@@ -24,4 +24,35 @@ defmodule Publisher do
   defp schedule_next_publish(interval) do
     Process.send_after(self(), :publish_data, interval)
   end
+
+  @impl true
+  def handle_info(:publish_data, state) do
+    {:noreply, state |> measure() |> publish()}
+  end
+
+  defp measure(state) do
+    measurements = Enum.reduce(state.sensors, %{}, fn sensor, acc ->
+      sensor_data = sensor.read.() |> sensor.convert.()
+      Map.merge(acc, sensor_data)
+    end)
+
+    %{state | measurements: measurements}
+  end
+
+  defp publish(state) do
+    result =
+      :post
+      |> Finch.build(
+        state.weather_tracker_url,
+        [{"Content-Type", "application/json"}],
+        Jason.encode!(state.measurements)
+      )
+      |> Finch.request(WeatherTrackerClient)
+
+    Logger.debug("Server response: #{inspect(result)}")
+
+    schedule_next_publish(state.interval)
+
+    state
+  end
 end
